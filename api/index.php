7e76d6c04a444dd06191b33aab9cd1e7291d8f89
Muhamad Yusril <muh.yusril098@gmail.com>
2026-02-28 12:08:06 +0700

    Force creation of storage/framework/views and bootstrap/cache inside Vercel's /tmp to fix 'class view does not exist' fatal error

--- api/index.php
+++ api/index.php
@@ -4,6 +4,17 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 try {
+    // Pastikan Vercel Temporary Folders (storage dan turunannya) telah dipaksa dibuat dan diberi izin tulis
+    $compiledPath = '/tmp/storage/framework/views';
+    $logsPath = '/tmp/storage/logs';
+    $appPath = '/tmp/bootstrap/cache';
+
+    foreach ([$compiledPath, $logsPath, $appPath] as $path) {
+        if (!is_dir($path)) {
+            mkdir($path, 0777, true);
+        }
+    }
+
     require __DIR__ . '/../public/index.php';
 } catch (\Throwable $e) {
     echo "<h1>Fatal Error dari Vercel:</h1>";
